9db1a4f60ec2bf49c7f0e41690efafa9042682f3
Leticia Sontay <lsontayc1@miumg.edu.gt>
2024-05-31 20:38:00 -0600

    Proy_funcionando

---
+++ PrototipoEFP32024pt2/src/main/java/com/mycompany/prototipoefp32024pt2/PrototipoEFP32024pt2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prototipoefp32024pt2;
+
+/**
+ *
+ * @author Leticia
+ */
+public class PrototipoEFP32024pt2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
